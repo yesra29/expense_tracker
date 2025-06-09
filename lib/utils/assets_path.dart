d7e25c7460487576a2be4f1c1d388484102089b8
Yesra Fatima <fyesra0@gmail.com>
2025-06-09 23:21:48 +0530

    app logo and name changed.

---
+++ lib/utils/assets_path.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF4CAF50);
+  static const Color secondry = Color(0xFFFFC107);
+  static const Color alert = Color(0xFFF44336);
+  static const Color income = Color(0xFF2196F3);
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color card = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color divider = Color(0xFFE0E0E0);
+  static const Color buttonBg = Color(0xFF4CAF50);
+  static const Color buttonText = Color(0xFFFFFFFF);
+}
